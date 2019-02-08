b83684adc89cbce867662252f80815d2518344af
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-02-08 09:48:00 -0500

    cleaned up comments

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,19 +1,14 @@
 package io.vertx.starter;
 
-import io.reactivex.Completable;
-import io.reactivex.Single;
-import io.vertx.core.json.JsonArray;
-import io.vertx.reactivex.CompletableHelper;
-import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
 import io.vertx.reactivex.ext.web.handler.StaticHandler;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 
 public class MainVerticle extends AbstractVerticle {
@@ -43,10 +38,6 @@ public class MainVerticle extends AbstractVerticle {
     apiRouter.get("/cacheControllers").handler(this::getCachesHandler);
 
     baseRouter.mountSubRouter("/api", apiRouter);
-
-//    Completable retrieveCache = CacheController.<String, String>create("localhost", vertx)
-//      .doOnSuccess(c -> this.cacheControllers.put(c.toString(), c))
-//      .toCompletable();
 
     vertx
       .createHttpServer()
@@ -100,67 +91,6 @@ public class MainVerticle extends AbstractVerticle {
           .end(this.caches.encodePrettily());
       }).subscribe();
     }
-
-    /*
-    Single.just(routingContext.getBodyAsJson())
-      .doOnSuccess(c -> {
-        String host = c.getString("host");
-        String cache = c.getString("cache");
-        LOGGER.finest(host);
-        LOGGER.finest(cache);
-        if(this.cacheControllers.containsKey(host)){
-          LOGGER.finest("cache controller contains host " + host);
-          this.caches.getJsonArray(host).add(cache);
-          LOGGER.finest(this.caches.getJsonArray(host).toString());
-          routingContext.response()
-            .setStatusCode(200)
-            .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-            .end(this.caches.encodePrettily());
-        }else{
-          CacheController.create(host, vertx).doOnSuccess(n -> {
-            LOGGER.finest("cache controller does not contains host " + host);
-            this.cacheControllers.put(host, n);
-            LOGGER.finest("added CacheController for host " + host);
-            this.caches.put(host, new JsonArray().add(cache));
-            LOGGER.finest(this.caches.getJsonArray(host).toString());
-            routingContext.response()
-              .setStatusCode(200)
-              .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-              .end(this.caches.encodePrettily());
-          }).doOnError(e -> {
-            routingContext.response()
-              .setStatusCode(500)
-              .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-              .end(new JsonObject().put("error", e.getMessage()).encodePrettily());
-          });
-        }
-      }).doOnError(e -> {
-      routingContext.response()
-        .setStatusCode(500)
-        .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-        .end(new JsonObject().put("error", e.getMessage()).encodePrettily());
-    }).subscribe();
-    Single.just(routingContext.getBodyAsJson().getJsonObject("cache"))
-      .doOnSuccess(c -> {
-        if(this.caches.containsKey(c.getString("host"))){
-          this.caches.getJsonArray(c.getString("host")).add(c.getString("cache"));
-        }else{
-          CacheController.create(c.getString("host"), vertx).doOnSuccess(n -> {
-            this.cacheControllers.put(c.getString("host"), n);
-            this.caches.put(c.getString("host"), new JsonArray().add(c.getString("cache")));
-          });
-        }
-        routingContext.response()
-          .setStatusCode(200)
-          .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-          .end(this.caches.encodePrettily());
-      }).doOnError(e -> {
-      routingContext.response()
-        .setStatusCode(500)
-        .putHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON)
-        .end(new JsonObject().put("error", e.getMessage()).encodePrettily());
-    }).subscribe();
-*/
   }
 
 }
